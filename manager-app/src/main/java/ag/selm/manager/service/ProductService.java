4b92da6678de876a8f95bce80197ccdffe914189
Maxim <official.showex@gmail.com>
2025-03-02 17:14:12 +0400

    basic web application

---
+++ manager-app/src/main/java/ag/selm/manager/service/ProductService.java
@@ -0,0 +1,19 @@
+package ag.selm.manager.service;
+
+import ag.selm.manager.entity.Product;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductService {
+
+    List<Product> findAllProducts();
+
+    Product createProduct(String title, String details);
+
+    Optional<Product> findProduct(int productId);
+
+    void updateProduct(Integer id, String title, String details);
+
+    void deleteProduct(Integer id);
+}
